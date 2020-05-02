4d07ef2d5be0c1457f68bec58ef9c5dd1efae3d4
DESKTOP-DELTEBT\KHIN YADANA PHYO <khinyadanaphyo@u.nus.edu>
2020-05-02 22:13:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ app.post('/register_books', (req,res)=> {
      
 
     // db.collection("Book").doc("collectionSu").set({genre:elements});
-     db.collection("Book").doc("collectionSu").collection("collectionInnwa").set({
+     db.collection("Book").doc("collectionSu").collection("collectionInnwa").add({
      	bshopaddress:bookshopaddress,
      	bookshopphno:bookshopphno,
      	stock:stock
